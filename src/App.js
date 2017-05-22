a82778ee1de6f10182065b72cd9fafcefe379ff8
zhufengnodejs <zhufengnodejs@126.com>
2017-05-22 21:34:11 +0800

    2. 引入bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
 import {
     BrowserRouter as Router,
     Route,
@@ -7,7 +8,7 @@ import {
 
 const Home = () => (
     <div>
-        <h2>个人主页</h2>
+        <h2>首页</h2>
     </div>
 )
 
@@ -17,26 +18,40 @@ const User = () => (
     </div>
 )
 
-const Profile = ({match}) => (
+const Profile = () => (
     <div>
-        <h3>{match.params.userId}</h3>
+        <h3>个人设置</h3>
     </div>
 )
 
 const App = () => (
     <Router>
         <div>
-            <ul>
-                <li><Link to="/">主页</Link></li>
-                <li><Link to="/user">用户管理</Link></li>
-                <li><Link to="/profile">个人设置</Link></li>
-            </ul>
-
-            <hr/>
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/user" component={User}/>
-            <Route path="/profile" component={Profile}/>
+            <div className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <div className="navbar-brand">
+                            <Link to="/">学生管理系统</Link>
+                        </div>
+                    </div>
+                    <div>
+                        <ul className="nav navbar-nav">
+                            <li><Link to="/">首页</Link></li>
+                            <li><Link to="/user">用户管理</Link></li>
+                            <li><Link to="/profile">个人设置</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/user" component={User}/>
+                        <Route path="/profile" component={Profile}/>
+                    </div>
+                </div>
+            </div>
         </div>
     </Router>
 )
