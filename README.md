d192769c917f8352717384abbe83d9b138c6783a
zhufengnodejs <zhufengnodejs@126.com>
2017-05-22 23:14:45 +0800

    6. 实现登录和退出功能

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ReactRouter4的课件
-
+https://reacttraining.com/react-router
 ## 内容大纲
 
 - 1. 搭建最简单的路由环境
@@ -7,3 +7,4 @@ ReactRouter4的课件
 - 3. 实现二级路由
 - 4. 获取路径参数
 - 5. switch只显示一个组件
+- 6. 实现登录和退出功能
--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import {
 import Home from './Home';
 import User from './User';
 import Profile from './Profile';
-
+import PrivateRoute from './PrivateRoute';
+import Login from './Login';
 const App = () => (
     <Router>
         <div>
@@ -28,15 +29,19 @@ const App = () => (
                             <li><Link to="/profile">个人设置</Link></li>
                             <li><Link to="/zhufengpeixun">品牌</Link></li>
                         </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><Route path="/" render={({history}) => (<a onClick={() => {localStorage.clear();history.push('/');}}>退出</a>)}/></li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <div className="container">
                 <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/user" component={User}/>
-                <Route path="/profile" component={Profile}/>
-                <Route path="/:name" render={({match})=>(<div>{match.params.name}</div>)}/>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/user" component={User}/>
+                    <PrivateRoute path="/profile" component={Profile}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/:name" render={({match}) => (<div>{match.params.name}</div>)}/>
                 </Switch>
             </div>
         </div>
